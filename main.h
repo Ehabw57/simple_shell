af8edd28284ebf50d12e53a338b08a75ee21bd88
eslam <eslam@gmail.com>
2023-09-17 01:08:21 -0700

    realloc done

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 int _strncmp(const char *dest, const char *source, unsigned int index);
 int _strcmp(const char *dest, const char *source);
 int _strlen(const char *source);
+char *_stringcpy(char *destination, char *source);
 int _varcmp(const char *var, const char *env);
 char *_store(char po, char y, unsigned int n);
 void deletee(char **p);
