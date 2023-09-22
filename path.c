a53d6d49e90263e741c9042563e4ea001026cb3f
eslam <eslam@gmail.com>
2023-09-22 05:56:19 -0700

    done

--- path.c
+++ path.c
@@ -1,18 +1,18 @@
 #include "shell.h"
 
 /**
- * cmd - determines if a file is an exe
- * @i: struct
- * @p: path
+ * is_cmd - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
  *
- * Return: 1 if true, 0 no
+ * Return: 1 if true, 0 otherwise
  */
-int cmd(info_t *i, char *p)
+int is_cmd(info_t *info, char *path)
 {
 	struct stat st;
 
-	(void)i;
-	if (!p || stat(p, &st))
+	(void)info;
+	if (!path || stat(path, &st))
 		return (0);
 
 	if (st.st_mode & S_IFREG)
@@ -23,64 +23,64 @@ int cmd(info_t *i, char *p)
 }
 
 /**
- * duplicates_characters - duplicates characters
- * @p: the PATH
- * @s: index
- * @t: index
+ * dup_chars - duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
  *
  * Return: pointer to new buffer
  */
-char *duplicates_characters(char *p, int s, int t)
+char *dup_chars(char *pathstr, int start, int stop)
 {
-	static xhar buffer[1024];
-	int x = 0, b = 0;
+	static char buf[1024];
+	int i = 0, k = 0;
 
-	for (b = 0, x = s; x < t; x++)
-		if (p[x] != ':')
-			buffer[b++] = p[x];
-	buffer[b] = 0;
-	return (buffer);
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
 }
 
 /**
- * find_path - finds this cmd in the PATH
- * @i: struct
- * @p: the PATH
- * @c: the cmd
+ * find_path - finds this cmd in the PATH string
+ * @info: the info struct
+ * @pathstr: the PATH string
+ * @cmd: the cmd to find
  *
  * Return: full path of cmd if found or NULL
  */
-char *find_path(info_t *i, char *p, char *c)
+char *find_path(info_t *info, char *pathstr, char *cmd)
 {
-	int x = 0, curr = 0;
+	int i = 0, curr_pos = 0;
 	char *path;
 
-	if (!p)
+	if (!pathstr)
 		return (NULL);
-	if ((_string_length(c) > 2) && is_string_starts_with(c, "./"))
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
 	{
-		if (cmd(i, c))
-			return (c);
+		if (is_cmd(info, cmd))
+			return (cmd);
 	}
 	while (1)
 	{
-		if (!p[x] || p[x] == ':')
+		if (!pathstr[i] || pathstr[i] == ':')
 		{
-			path = duplicates_characters(p, curr, x);
+			path = dup_chars(pathstr, curr_pos, i);
 			if (!*path)
-				_string_concatenates(path, c);
+				_strcat(path, cmd);
 			else
 			{
-				_string_concatenates(path, "/");
-				_string_concatenates(path, c);
+				_strcat(path, "/");
+				_strcat(path, cmd);
 			}
-			if (cmd(i, path))
+			if (is_cmd(info, path))
 				return (path);
-			if (!p[x])
+			if (!pathstr[i])
 				break;
-			curr = x;
+			curr_pos = i;
 		}
-		x++;
+		i++;
 	}
 	return (NULL);
 }
